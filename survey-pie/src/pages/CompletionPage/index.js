7be45a08c973c6f4c5fb4d5e3c2088baad6cab2c
WooJinYong <jinyongu@naver.com>
2024-11-04 21:21:36 +0900

    axios 사용해보기

--- survey-pie/src/pages/CompletionPage/index.js
+++ survey-pie/src/pages/CompletionPage/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 
@@ -7,12 +8,13 @@ import testWithComma from '../../stores/test/testWithComma';
 function CompletionPage() {
   const test = useRecoilValue(testWithComma);
 
+  axios.get('http://localhost:3000').then((res) => {
+    console.log('res', res);
+  });
+
   return <CompletionPageWrapper>{test}</CompletionPageWrapper>;
 }
 
-const CompletionPageWrapper = styled.div`
-  background: aqua;
-  padding: 4em;
-`;
+const CompletionPageWrapper = styled.div``;
 
 export default CompletionPage;
